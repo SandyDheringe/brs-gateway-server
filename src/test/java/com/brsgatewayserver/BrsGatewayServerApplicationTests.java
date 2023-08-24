7b62f34095826a4e816df720a337f79b9e6db898
sandeep <sandeep.ariseup@gmail.com>
2023-08-24 19:31:13 +0530

    initial commit

---
+++ src/test/java/com/brsgatewayserver/BrsGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brsgatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrsGatewayServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
