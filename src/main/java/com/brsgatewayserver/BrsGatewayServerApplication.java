9ea268dc8a93577614c35b81cb3e11fc6532f4b7
sandeep <sandeep.ariseup@gmail.com>
2023-08-31 01:14:30 +0530

    docker changes

--- src/main/java/com/brsgatewayserver/BrsGatewayServerApplication.java
+++ src/main/java/com/brsgatewayserver/BrsGatewayServerApplication.java
@@ -2,12 +2,18 @@ package com.brsgatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class BrsGatewayServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BrsGatewayServerApplication.class, args);
+//        SpringApplication.run(BrsGatewayServerApplication.class, args);
+
+        new SpringApplicationBuilder()
+                .profiles("dev")
+                .sources(BrsGatewayServerApplication.class)
+                .run(args);
     }
 
 }
